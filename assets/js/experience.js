4e5fc40fa1f9a07e827a2393b52813ddf7059bc2
Abinaya Mahendiran <abinaya.m02@mphasis.com>
2022-01-18 18:17:04 +0530

    Updated hackathons

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -116,40 +116,29 @@ document.addEventListener("DOMContentLoaded", showCards);
 const hackathonsection = document.querySelector(".hackathon-section");
 const mentor = [
   {
-    title: "The Uplift Project",
-    subtitle: "Mentor",
+    title: "Mphasis Hackathon Challenge",
+    subtitle: "Mphasis, 2021, Second Runner up",
+    image: "assets/images/experience-page/mphasis.jpg",
+    desp: "Built a platform to generate insights from multi-dimensional structured data using AI and present these insights in human readable and understandable formats like text, graph and charts using NLP.
+  },
+
+  {
+    title: "Digital Dutchess",
+    subtitle: "Sabre, 2017, Runner up",
     image: "assets/images/experience-page/uplift.png",
-    desp: "I would be responsible for guiding 100s of participants across the globe in their first steps towards Applied Data Science and building end-to-end machine learning projects.",
+    desp: "Built a solution to predict customer churn (R and Qilkview) for travel agencies as a part of the hackathon conducted by Sabre in association with Hackerearth.com. Won second place.
   },
   {
-    title: "ULHacks",
-    subtitle: "Judge",
+    title: "She-Hack",
+    subtitle: "Go-Jek, 2017, Runner up",
     image: "assets/images/experience-page/ulhacks.png",
-    desp: "ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
+    desp: "Built a solution to find the areas that has high demand for Go-Jek services and implemented a model to understand and improve the servicing rate by optimally allocating resources using Python and QlikView.",
   },
   {
-    title: "WaffleHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/wafflehacks.png",
-    desp: "WaffleHacks 2021 is an upcoming 48-hour student-organized hackathon with an aim to bring technical solutions to local communities and small businesses.",
-  },
-  {
-    title: "Elevate Tech",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/elevate.png",
-    desp: "Elevate Hacks is an upcoming all-female virtual hackathon coming from AUGUST 14th to 15th.",
-  },
-  {
-    title: "PitchTeen",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/pitchteen.png",
-    desp: "PitchTeen 2.0 was a tech startup competition for female and non-binary communities in high school and college from all over the world with an aim to create a safe and encouraging place for women to help them explore tech and business fields.",
-  },
-  {
-    title: "Hack-A-Solution",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/hackasolution.png",
-    desp: "Hack-a-Solution 2021 is a 24 hour long hackathon organized by Frisco students to raise awareness of world issues through technology.",
+    title: "HACKIT",
+    subtitle: "Cisco, 2015",
+    image: "assets/images/experience-page/cisco.jpg",
+    desp: "Built a recommender system using Python (scikit-learn) as a part of the internal hackathon to recommend skills for a designation as well as recommend people based on their skills to suit a designation.",
   },
 
 ];
